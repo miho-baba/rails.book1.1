73beb2c0f312baa5bf8643b75048156451f66908
EC2 Default User <ec2-user@ip-172-31-10-101.ap-northeast-1.compute.internal>
2023-09-02 12:38:35 +0000

    応用課題１完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,12 +7,17 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def new
+    @book = Book.new
+  end
+
   def create
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
+      @books = Book.all
       render :index
     end
   end
@@ -27,7 +32,7 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    if @book.update()
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'homes#top'
 
+  post 'books' => 'books#create'
   resources :books
+
 end
